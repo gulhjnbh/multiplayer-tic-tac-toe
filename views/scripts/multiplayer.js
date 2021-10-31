4441d6f692d4cc5a956a1ba566abf14aa5d00e46
Daichi Lee <singingbigsmart1111gamers@gmail.com>
2021-10-30 22:04:43 -0700

    change ui

---
+++ views/scripts/multiplayer.js
@@ -0,0 +1,23 @@
+const socket = io('http://localhost:3001');
+socket.on('connection')
+
+//code for multiplay chat (now is only send message text box)
+socket.on('message',  (data) => {
+    document.querySelector('p').innerText = data;
+});
+
+const sendMessage = () => {
+    const message = document.querySelector('.message').value;
+    socket.emit('message', message );
+}
+
+//code for multiplayer tic tac toe
+socket.on('board',  (board) => {
+    drawBoard(board);
+});
+const sendBoard = () => {
+    const board = getBoard();
+    socket.emit('board', board );
+}
+
+
